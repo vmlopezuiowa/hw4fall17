31c244792aa7a5e1220c3501356900071dd72dad
Vincent Lopez <vincent-lopez@uiowa.edu>
2017-09-29 05:08:52 +0000

    wip

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+    def user_params
+        params.require(:user).permit(:user_id, :email, :session_token, :timestamps)
+    end
+    
+    def show
+        id = params[:id] # retrieve user ID from URI route
+        @user = User.find(id) # look up user by unique ID
+        # will render app/views/user/show.<extension> by default
+    end
+    
+    def new
+        # default: render 'new' template
+    end
+    def create
+        @user = User.create!(user_params)
+        flash[:notice] = "#{@user.user_id} was successfully created."
+        redirect_to users_path
+    end
+    def edit
+        @user = User.find params[:id]
+    end
+
+    def update
+        @user = User.find params[:id]
+        @user.update_attributes!(user_params)
+        flash[:notice] = "#{@user.user_id} was successfully updated."
+        redirect_to user_path(@user)
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:notice] = "User '#{@user.user_id}' deleted."
+        redirect_to users_path
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809022253) do
+ActiveRecord::Schema.define(version: 20170929045141) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20150809022253) do
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "user_id"
+    t.string   "email"
+    t.string   "session_token"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
