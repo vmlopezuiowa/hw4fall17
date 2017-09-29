f173cc2edc77ee844f613184da7d25f3aae4b131
Vincent Lopez <vincent-lopez@uiowa.edu>
2017-09-29 05:54:06 +0000

    trying things..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     end
     
     def new
-        render :new_user_path
+        render :new
     end
     def create
         @user = User.create!(user_params)
