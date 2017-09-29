29c2d5e4fc8d08df70bac34418aa9302010f3e4b
Vincent Lopez <vincent-lopez@uiowa.edu>
2017-09-29 07:47:53 +0000

    testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,9 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
+        redirect_to 'index'
         format.json { render :show, status: :created, location: @user }
+        
       else
         render 'new'
       end
