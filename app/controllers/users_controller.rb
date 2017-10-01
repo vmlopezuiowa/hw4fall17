f43ef2c98955370c981dc18b79d5634553e86a7f
Vincent Lopez <vincent-lopez@uiowa.edu>
2017-10-01 21:20:00 +0000

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create!(user_params)
     flash[:notice] = "#{@user.user_id} was successfully created."
-    redirect_to :controller => "movie" , :action => "index"
+    redirect_to :controller => "movie" , :action => "movies_path"
   end
 
   # PATCH/PUT /users/1
