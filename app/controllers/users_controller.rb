6f1366156a6d038e2cc81de6a813ada1f064b9fc
Vincent Lopez <vincent-lopez@uiowa.edu>
2017-10-01 21:14:51 +0000

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:user_id, :email)
     end
 end
