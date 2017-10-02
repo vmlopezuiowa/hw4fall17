afed9ce4371f2c514880fe2f0db41599ec673779
Vincent Lopez <vincent-lopez@uiowa.edu>
2017-10-02 04:47:24 +0000

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,11 @@ Rails.application.routes.draw do
 
   resources :movies
   resources :users
+  
+  resources :sessions
+    match '/login', to: 'sessions#new', via: :get
+    match '/login_create', to: 'sessions#create', via: :post
+    match '/logout', to: 'sessions#destroy', via: :delete
   # Example resource route with options:
   #   resources :products do
   #     member do
