f38d6f6ccfbcce60cde4d97bb722ef8b3f622605
Vincent Lopez <vincent-lopez@uiowa.edu>
2017-10-02 02:35:43 +0000

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
     validates :user_id , uniqueness: true
         
-    def create_user(user_params)
+    def self.create_user(user_params)
         session_token = SecureRandom.base64
         ActiveRecord.create(user_params,session_token)
     end
